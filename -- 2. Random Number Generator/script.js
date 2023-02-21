04f5c77ccdf6c75e8f0641da12ff08cb6cfef1c6
Ricardo Rivera <hello@webbrah.com>
2023-02-20 18:55:19 -0600

    Initial Commit

---
+++ -- 2. Random Number Generator/script.js
@@ -0,0 +1,17 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => {
+    // Generate number between 1 and 10
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber;
+}
+
+btn.addEventListener("click", generateNumber);
+
+// Generate a random number on page load
+generateNumber();
+
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
