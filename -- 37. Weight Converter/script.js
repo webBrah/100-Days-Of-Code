6663c0187efbdf5d1bcbd0ecf33ec2168d69c19f
Ricardo Rivera <hello@webbrah.com>
2023-03-28 21:59:28 -0600

    Day 37

--- -- 37. Weight Converter/script.js
+++ -- 37. Weight Converter/script.js
@@ -16,23 +16,23 @@ function convertWeight(e) {
 
     if (e.target.classList.contains("kilograms")) {
         let x = e.target.value;
-        pounds.value = x * 2.2046;
-        grams.value = x * 1000;
-        ounces.value = x * 35.274;
+        pounds.value = (x * 2.2046).toFixed(2);
+        grams.value = (x * 1000).toFixed(2);
+        ounces.value = (x * 35.274).toFixed(2);
     }
 
     if (e.target.classList.contains("grams")) {
         let x = e.target.value;
-        kilograms.value = x / 1000;
-        pounds.value = x * 0.0022046;
-        ounces.value = x * 0.035274;
+        kilograms.value = (x / 1000).toFixed(2);
+        pounds.value = (x * 0.0022046).toFixed(2);
+        ounces.value = (x * 0.035274).toFixed(2);
     }
 
     if (e.target.classList.contains("ounces")) {
         let x = e.target.value;
-        kilograms.value = x / 35.274;
-        grams.value = x / 0.035274;
-        pounds.value = x * 0.0625;
+        kilograms.value = (x / 35.274).toFixed(2);
+        grams.value = (x / 0.035274).toFixed(2);
+        pounds.value = (x * 0.0625).toFixed(2);
     }
 }
 
