f64b44f93e73a32c3ae55f50f75f56f0a53f8a6d
Ricardo Rivera <hello@webbrah.com>
2023-03-30 15:22:53 -0600

    Day 39

---
+++ -- 39. Running Game/script.js
@@ -0,0 +1,30 @@
+const hero = document.querySelector('.hero'),
+  heroBoy = document.querySelector('.hero-boy'),
+  vilan = document.querySelector('.vilan');
+
+  const jump = () => {
+    if (hero.classList != "animate") {
+        hero.classList.add("animate");
+        vilan.style.animation = "move 1s infinite linear";
+    }
+    setTimeout( ()=> {
+        hero.classList.remove("animate");
+    }, 300);
+  };
+
+  document.addEventListener("keydown", (e) => {
+    if(e.code == "Space") {
+        jump();
+    }
+  });
+
+  let isAlive = setInterval(() => {
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    // console.log(heroTop);
+    let vilanLeft = parseInt(window.getComputedStyle(vilan).getPropertyValue("left"));
+
+    if (vilanLeft < 40 && vilanLeft > 20 && heroTop >= 130) {
+        vilan.style.animation = "none";
+        alert("Game over. Press spacebar to start");
+    }
+  }, 10);
